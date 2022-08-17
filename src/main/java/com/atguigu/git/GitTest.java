8a282ebc3ec5c81444a2ededde1c8b89c595deba
Jeffrey <806532727@qq.com>
2022-08-17 20:23:20 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @Date: 2022/8/17 0017 20:14:28
+ * @Author: jia
+ * @Description: TODO
+ * @Version: V1.0
+ **/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
